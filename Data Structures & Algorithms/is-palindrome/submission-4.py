212ecfa9d53d310e97d714b77599a1a5d20e770e
Isabel Agbu <127052341+isabelagbu@users.noreply.github.com>
2026-06-15 20:09:47 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # create cleaned_s variable
+        # loop through s
+            # cleaned_s += check if each is alnum 
+
+        # reverse cleaned_s
+        # check id cleaned_s == reversed
+
+        cleaned_s = ""
+        for char in s:
+            if char.isalnum():
+                cleaned_s += char
+        if cleaned_s.lower() == cleaned_s[::-1].lower():
+            return True
+        else:
+            return False
