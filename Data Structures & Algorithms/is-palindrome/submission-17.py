9f5bbeb9189a4c5fe31b822b889cf33e37ed8513
Isabel Agbu <127052341+isabelagbu@users.noreply.github.com>
2026-07-02 20:13:03 -0700

    Add: is-palindrome - submission-17

---
+++ Data Structures & Algorithms/is-palindrome/submission-17.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #two pointers. left. right
+        # while loop
+        # check if alnum. no? proceed until alnum
+        # check equality. no? return False
+
+        #time: O(n)
+        #space: 0(1)
+
+        leftindex = 0
+        rightindex = len(s) - 1
+
+        while leftindex < rightindex:
+            while s[leftindex].isalnum() == False and leftindex < rightindex:
+                leftindex += 1
+            while s[rightindex].isalnum() == False and leftindex < rightindex:
+                rightindex -= 1
+
+            if s[leftindex].lower() != s[rightindex].lower():
+                return False
+
+            leftindex += 1
+            rightindex -= 1
+
+        return True
+
+
+        
