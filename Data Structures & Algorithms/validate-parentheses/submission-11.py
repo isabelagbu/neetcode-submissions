855d546fbb24c843736c926702ce7eb88cd72ab7
Isabel Agbu <127052341+isabelagbu@users.noreply.github.com>
2026-07-03 01:51:58 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # initialize bracketdict
+        # create stack using array
+        # for loop
+            # if open bracket push to stack
+            # if close pop stack, make sure stack has chars. check if poped matches dict
+        # if stack empty return true. else. false
+
+        if len(s) < 2:
+            return False
+
+        brackets = {")":"(","}":"{","]":"["}
+        mystack = []
+
+        for i in range(len(s)):
+            if s[i] in ["(","{","["]:
+                mystack.append(s[i])
+            else:
+                if len(mystack) == 0:
+                    return False
+                if mystack.pop() != brackets[s[i]]:
+                    return False
+
+        if len(mystack) != 0:
+            return False
+        else:
+            return True
+        
