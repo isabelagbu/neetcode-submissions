5f0e2e5553357a6495bb94b1f7b42080f9914849
Isabel Agbu <127052341+isabelagbu@users.noreply.github.com>
2026-07-22 11:09:29 -0700

    Add: validate-parentheses - submission-20

---
+++ Data Structures & Algorithms/validate-parentheses/submission-20.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2:
+            return False
+        
+        brackets = {")":"(","]":"[","}":"{"}
+        mystack = []
+
+        for bracket in s:
+            if bracket in ["[","{","("]:
+                mystack.append(bracket)
+            else:
+                if len(mystack) == 0:
+                    return False
+                elif mystack.pop() != brackets[bracket]:
+                    return False
+
+        if len(mystack) == 0:
+            return True
+        else:
+            return False
